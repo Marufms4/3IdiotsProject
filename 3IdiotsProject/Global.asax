a9837a7dc940777c0875582004aa3aafbda4ce76
Maruf <marufsm4@gmail.com>
2023-12-03 16:33:55 +0600

    Init

---
+++ 3IdiotsProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3IdiotsProject.MvcApplication" Language="C#" %>
---
+++ 3IdiotsProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3IdiotsProject.Startup))]
+namespace _3IdiotsProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
